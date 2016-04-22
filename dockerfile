18e92decdbe6402b0f7552b6f65c86393a956a5a
Qi Liu <liuq901@163.com>
2016-04-22 15:23:35 +0800

    finish container communication

--- dockerfile
+++ dockerfile
@@ -2,4 +2,9 @@ FROM ubuntu
 
 MAINTAINER liuq901
 
-ENTRYPOINT python main.py
+RUN apt-get install python -y
+
+ADD main.py main.py
+ADD run.sh run.sh
+
+ENTRYPOINT ./run.sh
--- main.py
+++ main.py
@@ -18,14 +18,29 @@ def scale(port):
     if port == 8001:
         return 2.0
 
-def send(port, value):
+def send(ip, port, value):
     send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_socket.connect(('', port))
-    send_socket.sendall('comm /mem_use=' + str(value) + '!')
+    try:
+        send_socket.connect((ip, port))
+        send_socket.sendall('comm /mem_use=' + str(value) + '!')
+    except socket.error:
+        pass
     send_socket.close()
+
+def check(ip, port):
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    flag = True
+    try:
+        send_socket.connect((ip, port))
+        send_socket.sendall('test hello')
+    except socket.error:
+        flag = False
+    send_socket.close()
+    return flag
 
 port = int(sys.argv[1])
 mem = 0
+ip = 'localhost'
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind(('', port))
@@ -33,16 +48,21 @@ listen_socket.listen(1)
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    print request
     url = get_url(request)
     http_response = None
-    if url == '/mem_info':
+    if url.startswith('/set_ip'):
+        ip = re.match('/set_ip=(.*)', url).group(1)
+        if check(ip, port ^ 1):
+            http_response = 'set ip to %s' % ip
+        else:
+            http_response = 'ip error'
+    elif url == '/mem_info':
         http_response = 'memory used %d' % mem
     elif url.startswith('/mem_use'):
         mem = int(re.match('/mem_use=([0-9]*)', url).group(1))
         change_memory(mem)
         if url[-1] != '!':
-            send(port ^ 1, int(mem * scale(port)))
+            send(ip, port ^ 1, int(mem * scale(port)))
         http_response = 'change memory used to %d' % mem
     else:
         http_response = 'Hello, world!'
